3a0cd0f8a8b2d56ad2a973092957e926e72964dd
Naum Yashuvaev <hahalol915@gmail.com>
2021-02-13 21:53:11 +0300

    done

--- source/inverse.js
+++ source/inverse.js
@@ -13,19 +13,12 @@
  * 
  * @returns {numbers} - возвращается развернутый/частично развернутый массив
  */
-/*const inverse = (numbers, n = 0) => {
-    if (!Number.isInteger(n) || !Array.isArray(numbers))  {throw new TypeError('Ошибка входных данных');}
-    return (n >= 0) ? numbers.slice(0,n).concat(numbers.slice(n).reverse()) :
-        numbers.slice(0,numbers.length + n).reverse().concat(numbers.slice(n));
-};*/
 const inverse = (numbers, n = 0) => {
     if (!Number.isInteger(n) || !Array.isArray(numbers)) {
         throw new TypeError('Ошибка входных данных');
     }
-    if (n === 0) {
-        return numbers.reverse();
-    }
+    
     let buf = numbers.slice(0);
-    return (n > 0) ? [...numbers.splice(0,n), ...buf.splice(n).reverse()] :
+    return (n >= 0) ? [...numbers.splice(0,n), ...buf.splice(n).reverse()] :
         [...numbers.splice(0, numbers.length + n).reverse(), ...numbers.splice(n)];
 };
